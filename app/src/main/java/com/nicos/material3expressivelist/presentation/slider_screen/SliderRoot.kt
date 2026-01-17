e81568b51b6a6b7ffe8e3e215e1fa466537fb247
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-18 00:58:52 +0200

    create the class for slider

--- app/src/main/java/com/nicos/material3expressivelist/presentation/slider_screen/SliderRoot.kt
+++ app/src/main/java/com/nicos/material3expressivelist/presentation/slider_screen/SliderRoot.kt
@@ -1,10 +1,32 @@
 package com.nicos.material3expressivelist.presentation.slider_screen
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.nicos.material3expressivelist.presentation.CustomToolbar
 import com.nicos.material3expressivelist.presentation.navigation.navigation_3.Navigator
 
 @Composable
 fun SliderRoot(
     navigator: Navigator
 ) {
+    Scaffold(
+        topBar = {
+            CustomToolbar(
+                title = "Slider",
+                backButton = {
+                    navigator.goBack()
+                }
+            )
+        }
+    ) { paddingValues ->
+        SliderScreen(paddingValues = paddingValues)
+    }
+}
+
+@Composable
+private fun SliderScreen(
+    paddingValues: PaddingValues
+) {
+
 }
